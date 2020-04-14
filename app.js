e2dff41d82fac23e60d39c4fc9a6646f2c40210c
Daniel Duque <duque@outlook.com>
2020-04-14 15:35:40 -0500

    Installed missing dependencies, added error checking and mongoose connections to server

--- app.js
+++ app.js
@@ -1,20 +1,79 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config()
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const debug = require('debug')('the-word-game:server')
 
-var app = express();
+const createError = require('http-errors')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Routes
+const indexRouter = require('./routes/index')
+const usersRouter = require('./routes/users')
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Database connection
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    keepAlive: true
+  })
+  .then(x => {
+    debug(`Connected to MongoDB! Database name: '${x.connections[0].name}"`)
+  })
+  .catch(err => {
+    debug('Error connecting to MongoDB', err)
+  })
 
-module.exports = app;
+const app = express()
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static(path.join(__dirname, 'client/build')))
+
+// Session middleware
+app.use(
+  session({
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 24 * 60 * 60 // 1 day
+    }),
+    secret: process.env.SECRET_SESSION,
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000
+    }
+  })
+)
+
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+
+/* app.get('*', (req, res) => {
+  res.sendfile(path.join(__dirname, 'client/build/index.html'))
+}) */
+
+// Error handler
+app.use((err, req, res, next) => {
+  // Set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+  const status = err.status || 500
+  // Respond with error message
+  res.status(status).json(err.message)
+})
+
+module.exports = app
