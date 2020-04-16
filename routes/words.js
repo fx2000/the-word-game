d2ed966b31890636d5485bc1f0d9bba7815fdc95
Daniel Duque <duque@outlook.com>
2020-04-16 16:09:05 -0500

    Preliminary work in words API

--- routes/words.js
+++ routes/words.js
@@ -1,9 +1,25 @@
-var express = require('express')
-var router = express.Router()
+const express = require('express')
+const router = express.Router()
+const axios = require('axios')
+/* const wordnikApi = require('../lib/wordnikApi') */
 
 /* GET words listing. */
 router.get('/', function (req, res, next) {
-  res.send('respond with a resource')
+  const response = async () => {
+    try {
+      return await axios.get(process.env.WORDNIK_API_URL + 'words.json/randomWord', {
+        params: {
+          api_key: process.env.WORDNIK_API_KEY,
+          includePartOfSpeech: 'noun',
+          minCorpusCount: 8000,
+          maxCorpusCount: -1
+        }
+      })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+  res.status(200).json(response.data)
 })
 
 module.exports = router
